758f937398e6b07ade53fccc4bb07cafa916a0a4
Athistaraja <149251301+Athistaraja@users.noreply.github.com>
2024-02-10 19:17:02 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//! Promise task 2 is about fetch the data from the ISRO api ("https://isro.vercel.app/api/customer_satellites") and
+//! Promise task 1 is about fetch the data from the ISRO api ("https://isro.vercel.app/api/customer_satellites") and
 //! displaying Id, country, Launch-Date, Mass, Launcher by using Promise and Fetch.
 
 //! The div tag is slected by getElementsByClassName
